3e3ccf9b329700d7d5f51e7fa36af1c67a5d61ff
Kumail Ahmad <kumailahmad217@gmail.com>
2025-02-18 21:17:51 +0530

    created dashboard boxes

--- app/page.js
+++ app/page.js
@@ -1,23 +1,30 @@
 "use client";
+import { DashItems } from "./utilities/dash";
 import React, { useState } from "react";
 export default function Home() {
   return (
     <div>
-      <div className=" bg--200 ">
+      <div className="  ">
         <div className="items-end">UserProfile</div>
         <hr />
         <div>
           <h1 className="text-2xl font-bold ml-12 mt-4  ">Dashboard </h1>
-          <div className="boxes bg-red-400 ml-12 mt-5 flex w-[300px] h-[100px] rounded-lg">
-            <div className="box flex mt-5">
-              <div className="img ml-5">IC</div>
-              <div className="ml-7">
-                <div className="heading ">Total Students</div>
-                <div className="value">20%</div>
+          <div className="flex flex-col">
+            {DashItems.map((item) => (
+              <div key={item.id} className="container flex w-full">
+                <div className="boxes bg-blue-200 ml-12 mt-5 flex w-[300px] h-[100px] rounded-lg items-center  ">
+                  <div className="box flex items-center space-x-4">
+                    <div className="img ml-5">IC</div>
+                    <div className="ml-7 flex flex-col">
+                      <div className="heading text-lg">{item.title}</div>
+                      <div className="value text-lg">{item.percentage}%</div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        </div>
+        </div>{" "}
       </div>
     </div>
   );
---
+++ app/utilities/dash.js
@@ -0,0 +1,22 @@
+export const DashItems = [
+  {
+    id: 1,
+    title: "Total Students",
+    percentage: 10,
+    image: "",
+  },
+
+  {
+    id: 2,
+    title: "Total Present",
+    percentage: 10,
+    image: "",
+  },
+
+  {
+    id: 3,
+    title: "Total Absent",
+    percentage: 10,
+    image: "",
+  },
+];
