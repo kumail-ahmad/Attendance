e5fbce3f550efee0fe85c08a1143128761cf032a
Kumail Ahmad <kumailahmad217@gmail.com>
2025-02-21 08:25:18 +0530

    added count button-test

--- app/pages/student/page.js
+++ app/pages/student/page.js
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
 
 const page = () => {
-  return <div>i am a student testttt</div>;
+  const handleClick = () => {
+    alert("clicked");
+  };
+  return (
+    <div>
+      i am a student testttt
+      <button onClick={handleClick} className="border-red-800 p-3">
+        CLICK ME{" "}
+      </button>
+    </div>
+  );
 };
 
 export default page;
