c0f77938cb4739e6d066c60423bdcec802f04bc9
Kumail Ahmad <kumailahmad217@gmail.com>
2025-02-22 09:14:28 +0530

    tested count

--- app/pages/student/page.js
+++ app/pages/student/page.js
@@ -1,18 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [count, setCount] = useState(0);
   const handleClick = () => {
-    alert("clicked");
+    // alert("clicked");
+    setCount((count) => count + 1);
   };
   return (
     <div>
       i am a student testttt
       <button onClick={handleClick} className="border-red-800 p-3">
-        CLICK ME{" "}
+        CLICK ME {count}
       </button>
     </div>
   );
 };
 
-export default page;
+export default Page;
