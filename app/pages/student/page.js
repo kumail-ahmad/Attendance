484fcf47ecb21da4e6c1ff6bed072c05c9bc41e7
Kumail Ahmad <kumailahmad217@gmail.com>
2025-02-23 21:01:53 +0530

    designed button count

--- app/pages/student/page.js
+++ app/pages/student/page.js
@@ -8,11 +8,17 @@ const Page = () => {
     setCount((count) => count + 1);
   };
   return (
-    <div>
+    <div className="ml-12 mt-4 gap-4">
       <div>i am a student testttt</div>
-      <button onClick={handleClick} className="border border-red-800 p-3">
-        CLICK ME {count}
+      <button
+        onClick={handleClick}
+        className="border rounded-3xl bg-blue-600 border-red-800 p-3"
+      >
+        Add Student +
       </button>
+      <div className="bg-red-200 w-40 mt-5 items-center justify-center rounded-3xl">
+        {count}
+      </div>
     </div>
   );
 };
