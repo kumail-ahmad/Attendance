e57507cbf84e4458dc228c3ef03c25e27ed10ee3
Kumail Ahmad <kumailahmad217@gmail.com>
2025-02-24 20:53:46 +0530

    added student details

--- app/pages/student/page.js
+++ app/pages/student/page.js
@@ -8,16 +8,21 @@ const Page = () => {
     setCount((count) => count + 1);
   };
   return (
-    <div className="ml-12 mt-4 gap-4">
-      <div>i am a student testttt</div>
-      <button
-        onClick={handleClick}
-        className="border rounded-3xl bg-blue-600 border-red-800 p-3"
-      >
-        Add Student +
-      </button>
-      <div className="bg-red-200 w-40 mt-5 items-center justify-center rounded-3xl">
-        {count}
+    <div>
+      <div className="ml-12 mt-4 gap-4 flex justify-between m-4">
+        <div className=" mt-5 items-center justify-center rounded-3xl">
+          Total Student Count : {count}
+        </div>
+        <button
+          onClick={handleClick}
+          className="border rounded-3xl w-40 bg-purple-400 p-3 text-white font-bold"
+        >
+          Add Student +
+        </button>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold ml-12">Students</h1>
+        <input type="search" name="searchStu" id="srcStu" className="border-black bg-purple-300 p-3 ml-12 mt-4 w-72 h-9 rounded-lg" />
       </div>
     </div>
   );
