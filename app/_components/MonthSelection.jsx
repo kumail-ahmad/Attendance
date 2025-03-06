3eb1577a6b4e6099438c854cee63e18922af8e72
Kumail Ahmad <kumailahmad217@gmail.com>
2025-03-06 22:30:47 +0530

    added features to attendance page

--- app/_components/MonthSelection.jsx
+++ app/_components/MonthSelection.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client";
+import { useState, React } from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { CalendarDays } from "lucide-react";
+import { addMonths } from "date-fns";
+import moment from "moment";
 
 const MonthSelection = () => {
+  const today = new Date();
+
+  const nextMonth = addMonths(new Date(), 0);
+  const [month, setMonth] = useState(nextMonths);
   return (
     <div>
-      hey
-    </div>
-  )
-}
+      <Popover>
+        <PopoverTrigger>
+          <Button
+            variant="outline"
+            className="flex gap-2 items-center text-slate-500"
+          >
+            <CalendarDays className="h-5 w-5" />
+            {moment(month).format("MMM yyyy")}
+          </Button>
+        </PopoverTrigger>
 
-export default MonthSelection
+        <PopoverContent>Place content for the popover here.</PopoverContent>
+      </Popover>
+    </div>
+  );
+};
+
+export default MonthSelection;
